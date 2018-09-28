5acc28489f7706951b972d84bc11805093250c31
League <league@WTS9.attlocal.net>
2018-09-28 16:58:42 -0700

    PiAloud and Happy Pet

--- src/charAt_scanners_optionDialogs/PiAloud.java
+++ src/charAt_scanners_optionDialogs/PiAloud.java
@@ -1,7 +1,7 @@
 package charAt_scanners_optionDialogs;
 //    Copyright (c) The League of Amazing Programmers 2013-2017
-//    Level 0
 
+//    Level 0
 
 import java.io.IOException;
 import java.util.Scanner;
@@ -10,21 +10,29 @@ public class PiAloud {
 
 	// 1. Make a main method and make sure your program can run
 	public static void main(String[] args) {
-		
+
+		// 2. Make a String variable to hold the value of Pi. You could use
+		// http://www.piday.org/million/ for the value.
+		String Pi = "3.141592653589";
+		// 3. Print out some digits of Pi. The first value is "pi.charAt(0)", the second
+		// is "pi.charAt(1)"
+		/// System.out.println(Pi.charAt(0));
+		/// System.out.println(Pi.charAt(1));
+		// 4. Print ALL the digits of of Pi (hint: use a loop)
+		for (int i = 0; i < Pi.length(); i++) {
+			System.out.println(Pi.charAt(i));
+		}
+		// 5. Use the speak() method to speak all the digits of Pi.
+		for(int k = 0; k< Pi.length(); k++) {
+			speak(Pi.charAt(k));
+		}
+           
 	}
-
-	// 2. Make a String variable to hold the value of Pi. You could use http://www.piday.org/million/ for the value.
-
-	// 3. Print out some digits of Pi. The first value is "pi.charAt(0)", the second is "pi.charAt(1)"
-
-	// 4. Print ALL the digits of of Pi (hint: use a loop)
-
-	// 5. Use the speak() method to speak all the digits of Pi.
-
 	// [ADVANCED]
 	// *6. Get a character from the user using the getInputFromUser() method
 	// *7. Compare the users' char to the next digit of Pi
-	// *8. If they are correct, print out "correct". If they are not, print "incorrect" to System.err.println
+	// *8. If they are correct, print out "correct". If they are not, print
+	// "incorrect" to System.err.println
 
 	static void speak(char characterToSpeak) {
 		try {
@@ -42,12 +50,9 @@ public class PiAloud {
 		}
 	}
 
-
 	static char getInputFromUser() {
 		Scanner scanner = new Scanner(System.in);
 		return scanner.next().toCharArray()[0];
 	}
 
 }
-
-
