0d239251960194c29e6bae00a2235199f24ce3d3
League <league@WTS9.attlocal.net>
2018-10-04 19:57:11 -0700

    HappyPet and Own_Adventure

---
+++ src/charAt_scanners_optionDialogs/Own_Adventure.java
@@ -0,0 +1,9 @@
+package charAt_scanners_optionDialogs;
+
+import javax.swing.JOptionPane;
+
+public class Own_Adventure {
+public static void main(String[] args) {
+String adventure = JOptionPane.showInputDialog("You are stranded on an island in the sun and are extremly thirst and hungrey. You come across a goat that has a arrow ")
+}
+}
